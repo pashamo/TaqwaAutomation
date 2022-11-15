f6cfee730b7f01e7796cfe875be901cd8500d220
Mohammed Pasha <mk.pasha@hotmail.com>
2022-11-15 15:16:31 -0500

    feat: multi upload and log

--- vimeoFlow.py
+++ vimeoFlow.py
@@ -2,37 +2,63 @@ import yaml
 import time
 import vimeo
 import json
+import os
 
 config = yaml.load(open('config.yml'), Loader=yaml.FullLoader)
-API_TOK = config['network']['vimeo_api']['token']
-API_KEY = config['network']['vimeo_api']['id']
-API_SEC = config['network']['vimeo_api']['secret']
-FOLDER = "./downloads/"
-file_name = "TaqwaAutomation_1.1.0_setup.mp4"
-
+API_TOK = config['network']['vimeo_pasha_api']['token']
+API_KEY = config['network']['vimeo_pasha_api']['id']
+API_SEC = config['network']['vimeo_pasha_api']['secret']
+# API_TOK = config['network']['vimeo_api']['token']
+# API_KEY = config['network']['vimeo_api']['id']
+# API_SEC = config['network']['vimeo_api']['secret']
+downloaded_files = os.listdir("./downloads")
+upload_list = []
+download_dir = "./downloads/"
+log = "VIMEO UPLOADS LOG:\n"
 client = vimeo.VimeoClient(
     token=API_TOK,
     key=API_KEY,
     secret=API_SEC
 )
 
-uri = client.upload(FOLDER+file_name, data={
-  'name': file_name[:-4],
-  'description': 'Tutorial for v1.1.0'
-})
-print('Your video URI is: %s\n\n' % (uri))
+def uploadQuick(video):
+    global log
+    uri = client.upload(download_dir+video, data={
+    'name': "###TEST###"+video[:-4],
+    'description': "###TEST###"
+    })
+    response = client.get(uri + '?fields=link').json()
 
-while True:
-    response = client.get(uri + '?fields=transcode.status').json()
-    if response['transcode']['status'] == 'complete':
-        print('Your video finished transcoding.')
-        break
-    elif response['transcode']['status'] == 'in_progress':
-        print('Your video is still transcoding.')
-    else:
-        print('Your video encountered an error during transcoding.')
-        break
-    time.sleep(10)
+    print("Video uploaded.\nYour video URI is: %s" % (uri))
+    print("Your video link is: %s" % response['link'],"\n")
+    
+    log += "\nfile: " + video
+    log += "\nVideo uploaded.\nYour video URI is: " + uri
+    log += "\nYour video link is: " + response['link'] + "\n"
 
-response = client.get(uri + '?fields=link').json()
-print("\n\nYour video link is: %s" % response['link'])
+def uploadVideos(videos):
+    for video in videos:
+        print("> ",video)
+        uploadQuick(video)
+    print("Uploads have completed.")
+
+def getVideosList():
+    return list(filter(lambda x: x.endswith(".mp4"), downloaded_files))
+
+def writeLog():
+    fp = open(download_dir+"log_vimeoFlow.txt", 'w')
+    fp.write(log)
+    fp.close()
+
+def printUploads(): # utility to print recordings for upload
+    print('- - - - -')
+    print(str(len(upload_list)) + " recording(s) to upload:")
+    for recording in upload_list:
+        print("> ",recording)
+        time.sleep(0.25)
+    print('- - - - -')
+
+upload_list = getVideosList()
+printUploads()
+uploadVideos(upload_list)
+writeLog()
